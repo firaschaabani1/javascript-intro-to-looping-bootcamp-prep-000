62a7db26c726733671cca633fe6f53490b644bf5
Firas Chaabani <firaschaabani1@gmail.com>
2019-10-19 20:53:20 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,14 @@
+var i {0, ...25}
 
   for (let i = 0; i < 25; i++) function (forloop ){
     console.log ("I am"{i} "strange loop" )
   }
-
+var n {n > 0}
     while (n !== 0; n--) {
 
 console.log (n)
     }
+var num (num )
+while (num) {
+  
+}
