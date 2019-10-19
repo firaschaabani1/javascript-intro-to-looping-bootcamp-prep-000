34f7e478ed543982bb04a6ccceba5a8815c6cdf5
Firas Chaabani <firaschaabani1@gmail.com>
2019-10-19 21:03:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,32 +1,31 @@
 function forLoop(array) {
-
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.push("I am 1 strange loop.");
-    } else {
-      array.push(`I am ${i} strange loops.`);
-    }
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
   }
   return array;
 }
 
-//WHILE Loop
-function whileLoop(n) {
-  while (n > 0) {
-    console.log(--n);
+function whileLoop(number) {
+
+    while (number > 0) {
+      console.log(--number)
     }
-  return "done";
-}
+    return "done";
+  }
+  
 
-//DOWHILE Loop
 function doWhileLoop(array) {
-
-  function maybeTrue() {
-    return Math.random() >= 0.5;
+ function maybeTrue(){
+   return Math.random() >= 0.5
 }
 
-  do {
-    array.shift();
-  } while (array.length > 0 && maybeTrue());
+do {
+  array.pop();
+} while (array.length > -1 && maybeTrue());
   return array;
 }
+
